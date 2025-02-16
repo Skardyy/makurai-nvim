2e94e166b145b2a4f247ccc6d93fbbe8a32c5ec5
Meron Main PC <meronb1312@gmail.com>
2025-02-16 19:12:22 +0200

    fixing bad lua code. * theme can now be reassigned. * doesn't call lualine setup, but rather just adds to options

--- lua/makurai/init.lua
+++ lua/makurai/init.lua
@@ -1,13 +1,15 @@
-require("makurai.theme")
+local M = {}
 
--- Lualine
-local ok, lualine = pcall(require, "lualine")
-if ok then
-  lualine.setup({
-    options = {
-      theme = require("makurai.plugins.lualine"),
-    },
-  })
+function M.load()
+  require("makurai.theme").load()
+
+  -- Lualine
+  local ok, lualine = pcall(require, "lualine.config")
+  if ok then
+    local lualine_config = lualine.get_config()
+    lualine_config.options.theme = require("makurai.plugins.lualine")
+    lualine.apply_configuration(lualine_config)
+  end
 end
 
-return {}
+return M
